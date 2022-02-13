ec3bbc2b415a15929fe5a9f2d55dc15e25216d98
zoujinglong666 <675350014@qq.com>
2022-02-13 15:27:47 +0800

    引入京东UI组件库

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import {
 } from "vue";
 import App from "./App.vue";
 import { setupRouter } from './router' // 引入路由
+import NutUI from "@nutui/nutui";
+import "@nutui/nutui/dist/style.css";
 export function createApp() {
 	const app = createSSRApp(App);
 	setupRouter(app)
+	app.use(NutUI)
 	return {
 		app,
 	};
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,16 @@
 <template>
   <view class="content">
-    
     <view class="text-area">
       <text class="title">{{ title }}</text>
+      <nut-button type="primary">主要按钮</nut-button>
+      <nut-button type="info">信息按钮</nut-button>
+      <nut-button type="default">默认按钮</nut-button>
+      <nut-button type="danger">危险按钮</nut-button>
+      <nut-button type="warning">警告按钮</nut-button>
+      <nut-button type="success">成功按钮</nut-button>
     </view>
   </view>
+
 </template>
 
 <script setup>
